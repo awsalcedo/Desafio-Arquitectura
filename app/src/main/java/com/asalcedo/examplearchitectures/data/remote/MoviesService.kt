4e98d49ce3f731b9b33a2cb672b0196d4d63fb72
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2023-10-06 19:59:19 -0500

    Update api key TMDB

--- app/src/main/java/com/asalcedo/examplearchitectures/data/remote/MoviesService.kt
+++ app/src/main/java/com/asalcedo/examplearchitectures/data/remote/MoviesService.kt
@@ -3,6 +3,6 @@ package com.asalcedo.examplearchitectures.data.remote
 import retrofit2.http.GET
 
 interface MoviesService {
-    @GET("/discover/movie?api_key=5455465464556456")
+    @GET("discover/movie?api_key=368cc7d298097b7d0ae2b2165330d469")
     suspend fun getMovies() : MovieResult
 }
